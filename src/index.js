ff6ff7f06f327a25814eae04115e17cfa05a0ca9
Anastasiia Tychkova <tychkovaam@gmail.com>
2020-10-11 23:37:23 -0700

    first check

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
-import "./style.css";
+import "../src/styles/index.css";
+
 
 const numbers = [2, 3, 5];
 const doubledNumbers = numbers.map(number => number * 2); // Стрелочная функция. Не запнётся ли на ней Internet Explorer
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const webpack = require('webpack');
@@ -8,7 +9,10 @@ const isDev = process.env.NODE_ENV === 'development';
 const styleLoader = isDev ? 'style-loader' : MiniCssExtractPlugin.loader;
 
 module.exports = {
-  entry: {main: './src/index.js'},
+  entry: {
+    index: './src/index.js',
+    articles: './src/articles.js'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[chunkhash].js'
@@ -22,20 +26,45 @@ module.exports = {
           loader: "babel-loader"
         }
       },
+      //{
+        //test: /\.css$/, // применять это правило только к CSS-файлам
+        //use: [MiniCssExtractPlugin.loader, 'css-loader'] // к этим файлам нужно применить пакеты, которые мы уже установили
+      //},
       {
-        test: /\.css$/, // применять это правило только к CSS-файлам
-        use: [MiniCssExtractPlugin.loader, 'css-loader'] // к этим файлам нужно применить пакеты, которые мы уже установили
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../',
+            },
+          },
+          'css-loader',
+          'postcss-loader',
+        ],
       },
       {
-        test: /\.css$/i, // применять это правило только к CSS-файлам
-        use: [styleLoader,
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
           {
-            loader: 'css-loader',
+            loader: 'file-loader',
             options: {
-              importLoaders: 2,
+              name: '[name].[ext]',
+              publicPath: 'images',
+              outputPath: 'images',
+              useRelativePath: true,
+              esModule: false,
             }
-          }, 'postcss-loader']
-        // к этим файлам нужно применить пакеты, которые мы уже установили
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {}
+          },
+        ]
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/[name].[ext]&publicPath=../',
       },
     ]
   },
@@ -46,9 +75,29 @@ module.exports = {
     new HtmlWebpackPlugin({ // настроили плагин
       inject: false,
       template: './src/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
     }),
-    new WebpackMd5Hash()
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/articles.html',
+      filename: 'articles.html',
+    }),
+    new WebpackMd5Hash(),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: cssnano,
+      cssProcessorPluginOptions: {
+        preset: ['default', {
+          discardComments: {
+            removeAll: true
+          },
+          canPrint: true
+        }]
+      },
+    }),
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
   ]
 }
 
