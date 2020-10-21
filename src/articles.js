585b5e47f539c3ee83d52c3ed4e59f7052c4124f
Anastasiia Tychkova <tychkovaam@gmail.com>
2020-10-20 20:03:15 -0700

    for the second review: hamburger menu, grid for articles and mobile version

--- src/articles.js
+++ src/articles.js
@@ -1,1 +1,27 @@
 import "../src/styles/articles.css";
+
+const hamburger = document.getElementById('hamburgerWhite');
+const navigationMenu = document.querySelector('.header__menu');
+const header = document.querySelector('.header');
+const headerItems = document.querySelectorAll('.header__item_dark');
+const currentHeaderItem = document.getElementById('currentSelect');
+const mql = window.matchMedia('(min-width:708px)');
+
+
+hamburger.addEventListener('click', (e) =>{
+
+  header.classList.toggle('white')
+  headerItems.forEach(item => item.classList.remove('header__item_select_dark'));
+  navigationMenu.classList.toggle('show');
+
+})
+
+function removeHamburger(e) {
+  if (e.matches) {
+    header.classList.remove('black')
+    headerItem.classList.remove('header__item_select_dark')
+    navigationMenu.classList.remove('show');
+  }
+}
+
+mql.addEventListener('change', removeHamburger);
